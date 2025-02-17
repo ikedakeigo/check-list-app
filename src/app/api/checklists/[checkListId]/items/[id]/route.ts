8bf33fdd8189cb46b2f2c00cc315a361c60d51bb
silverskg <silverskg555@gmail.com>
2025-02-17 21:02:07 +0900

    チェックリスト更新、削除のAPI 実装

---
+++ src/app/api/checklists/[checkListId]/items/[id]/route.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+// チェックリストのアイテム更新
+export const PATCH = async (
+  req: NextRequest,
+  { params }: { params: { checkListId: string; id: string } }
+) => {
+  try {
+    const body = await req.json();
+    const { name, description, categoryId, quantity, unit, memo, status } = body;
+
+    const item = await prisma.checkListItem.update({
+      where: {
+        id: parseInt(params.id),
+        checkListId: parseInt(params.checkListId)
+      },
+      data: {
+        name,
+        description,
+        categoryId,
+        quantity,
+        unit,
+        memo,
+        status,
+        completedAt: status === 'Completed' ? new Date() : null
+      },
+      include: {
+        category: true // アイテムに関連するカテゴリー情報も取得
+      }
+    })
+
+    return NextResponse.json(item, { status: 200 })
+  } catch (error) {
+    console.error('Error:', error)
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    )
+  }
+}
+
+// チェックリストアイテムの削除
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { checkListId: string; id: string } }
+) => {
+  try {
+    await prisma.checkListItem.delete({
+      where: {
+        id: parseInt(params.id),
+        checkListId: parseInt(params.checkListId)
+      }
+    })
+
+    return NextResponse.json(null, { status: 200 })
+  } catch (error) {
+    console.error('Error:' , error)
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    )
+  }
+}
