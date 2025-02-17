8f01b5d2d7170ee3a250217a84e8f30d16ff75a5
silverskg <silverskg555@gmail.com>
2025-02-17 21:02:29 +0900

    チェックリスト更新、削除のAPI 実装

---
+++ src/app/api/checklists/[checkListId]/items/[id]/status/route.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+// チェックアイテムのステータス更新
+export const PATCH = async (
+  req: NextRequest,
+  { params }: { params: { checkListId: string, id: string } }
+) => {
+  try {
+    const body = await req.json();
+    const { status } = body;
+    const item = await prisma.checkListItem.update({
+      where: {
+        id: parseInt(params.id),
+        checkListId: parseInt(params.checkListId)
+      },
+      data: {
+        status,
+        completedAt: status === 'Completed' ? new Date() : null
+      }
+    })
+
+    return NextResponse.json(item, { status: 200 })
+  } catch (error) {
+    console.error('Error:', error)
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    )
+  }
+}
