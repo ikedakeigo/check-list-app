0a6619da59034775473e369f36b41b7ed1ce2c9a
silverskg <silverskg555@gmail.com>
2025-02-19 20:38:29 +0900

    supabaseよりログイン状態をチェックするカスタムhookを作成

---
+++ src/app/_hooks/useSupabaseSession.ts
@@ -0,0 +1,30 @@
+import { supabase } from "@/lib/supabase"
+import { Session } from "@supabase/supabase-js"
+import { useEffect, useState } from "react"
+
+export const useSupabaseSession = () => {
+  /**
+   * undifind: ログイン状態ロード中
+   * null: ログインしてない,
+   * Session: ログインしている // supabaseでsession情報を管理
+   */
+  const [session, setSession] = useState<Session | null | undefined>(undefined)
+  const [token, setToken] = useState<string | null>(null)
+  const [isLoding, setIsLoding] = useState(true)
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession()
+      setSession(session)
+      setToken(session?.access_token || null)
+      setIsLoding(false)
+    }
+
+    fetcher();
+  }, [])
+
+
+  return { session, token, isLoding }
+}
