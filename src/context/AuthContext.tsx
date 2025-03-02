080bad03b02144fb7dedbea1eebee0ff2abb7716
silverskg <silverskg555@gmail.com>
2025-03-02 20:06:25 +0900

    Contextを使用してアプリ全体で認証状態を管理するように設定

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { supabase } from '@/lib/supabase';
+import { User } from '@supabase/supabase-js';
+import { useRouter } from 'next/navigation';
+import { createContext, useContext, useEffect, useState } from 'react'
+
+type AuthContextType = {
+  user: User | null;
+  loading: boolean;
+  signOut: () => Promise<void>;
+  getToken: () => Promise<string | null>;
+};
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+  signOut: async () => {},
+  getToken: async () => null,
+});
+
+export const useAuth = () => useContext(AuthContext)
+
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    // 初期ロード時にsessionを取得
+    const getIntialSession = async () => {
+      try {
+        const { data: { session } } = await supabase.auth.getSession();
+        setUser(session?.user ?? null);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error('Error fetching session', error.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getIntialSession();
+
+    // 認証状態変更時のリスナーを登録
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        setUser(session?.user ?? null);
+        setLoading(false);
+      }
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    }
+  }, []);
+
+
+  // サインアウト
+  const signOut = async () => {
+    try {
+      await supabase.auth.signOut();
+      router.push('/login');
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error('Error signing out', error.message);
+      }
+    }
+  };
+
+  // トークン取得
+  const getToken = async () => {
+    try {
+      const { data } = await supabase.auth.getSession();
+      return data.session?.access_token ?? null;
+    } catch (error) {
+      console.error('Error getting token:', error);
+      return null;
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, signOut, getToken }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
