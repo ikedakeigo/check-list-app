15fa44e84c9872e9cd41906e8fa34c77fa1aa81b
silverskg <silverskg555@gmail.com>
2025-03-09 13:00:21 +0900

    アイコンコンポーネントの作成

---
+++ src/components/icons/PlusIcon.tsx
@@ -0,0 +1,7 @@
+const PlusIcon = ({ className="w-6 h-6"}: { className?: string }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+  </svg>
+);
+
+export default PlusIcon;
