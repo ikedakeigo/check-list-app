78435d79d40980255912433316f374f4fea33f99
silverskg <silverskg555@gmail.com>
2025-03-29 18:23:34 +0900

    チェックリストのリクエストbodyの型を修正

--- src/app/_types/checklists.ts
+++ src/app/_types/checklists.ts
@@ -1,7 +1,11 @@
+import { ChecklistStatus } from "./checkListItems";
+
 export type CheckListRequestBody = {
   name: string;
   description?: string;
   workDate: Date;
   siteName: string;
   isTemplate: boolean;
+  status: ChecklistStatus;
+  completedAt?: Date | null;
 }
