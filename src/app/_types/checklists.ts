3da4d3b75f4fabed1c48ebe745501508f61c109e
silverskg <k.grid.dev.555@gmail.com>
2025-04-04 12:34:10 +0900

    chekclistformdataの型を定義

--- src/app/_types/checklists.ts
+++ src/app/_types/checklists.ts
@@ -9,3 +9,12 @@ export type CheckListRequestBody = {
   status: ChecklistStatus;
   completedAt?: Date | null;
 }
+
+
+export type ChecklistFormData = {
+  name: string;
+  description?: string;
+  siteName: string;
+  workDate: Date;
+  isTemplate: boolean;
+}
