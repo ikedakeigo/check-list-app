96800afc03da8d3faf681b61d665e83891666422
silverskg <k.grid.dev.555@gmail.com>
2025-05-26 21:28:13 +0900

    withPWA()の中にnextConfigを渡す順番修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,39 @@
 // next.config.mjs
-import withPWA from 'next-pwa';
+import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const baseConfig = {
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-const pwaConfig = {
-  dest: 'public',
+const withPwa = withPWA({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-};
-
-export default withPWA({
-  ...baseConfig,
-  pwa: pwaConfig,
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest.json$/],
+  publicExcludes: ["!noprecache/**/*"],
+  fallbacks: {
+    document: "/offline", // オフライン時のフォールバックページ
+  },
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "https-calls",
+        networkTimeoutSeconds: 15,
+        expiration: {
+          maxEntries: 150,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30日
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
 });
+
+export default withPwa(nextConfig);
