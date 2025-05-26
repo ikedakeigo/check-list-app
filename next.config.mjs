655e34e61557b3f8019f811606b0eada08513e1d
silverskg <k.grid.dev.555@gmail.com>
2025-05-26 21:17:52 +0900

    reactStrictMode は pwa オプションの外側に

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,19 @@
 import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const baseConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+};
+
 export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
+  ...baseConfig,
+  pwa: pwaConfig,
 });
