fc23ef5b78b34dbf299a6dcfecd72a40a9c0ed87
silverskg <k.grid.dev.555@gmail.com>
2025-12-23 20:23:06 +0900

    [fix] コードの整形と一貫性を向上させるためにセミコロンを追加

--- src/app/_hooks/useSupabaseSession.ts
+++ src/app/_hooks/useSupabaseSession.ts
@@ -1,6 +1,6 @@
-import { supabase } from "@/lib/supabase"
-import { Session } from "@supabase/supabase-js"
-import { useEffect, useState } from "react"
+import { supabase } from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
 export const useSupabaseSession = () => {
   /**
@@ -8,23 +8,23 @@ export const useSupabaseSession = () => {
    * null: ログインしてない,
    * Session: ログインしている // supabaseでsession情報を管理
    */
-  const [session, setSession] = useState<Session | null | undefined>(undefined)
-  const [token, setToken] = useState<string | null>(null)
-  const [isLoding, setIsLoding] = useState(true)
+  const [session, setSession] = useState<Session | null | undefined>(undefined);
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoding, setIsLoding] = useState(true);
 
   useEffect(() => {
     const fetcher = async () => {
+
       const {
         data: { session },
-      } = await supabase.auth.getSession()
-      setSession(session)
-      setToken(session?.access_token || null)
-      setIsLoding(false)
-    }
+      } = await supabase.auth.getSession();
+      setSession(session);
+      setToken(session?.access_token || null);
+      setIsLoding(false);
+    };
 
     fetcher();
-  }, [])
+  }, []);
 
-
-  return { session, token, isLoding }
-}
+  return { session, token, isLoding };
+};
